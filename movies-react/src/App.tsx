24cb6ca15cc3596470d082239f1e324caa09be9a
Mahmudul Rafi <rafi.mahmudul.cse@gmail.com>
2022-09-14 15:27:51 +0600

    frontend-end-modified

--- movies-react/src/App.tsx
+++ movies-react/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import IndividualMovie from './components/movies/IndividualMovie';
+import { movieDTO } from './components/movies/movie.modal';
 
 function App() {
+
+  const testMovie: movieDTO = {
+    id: 1,
+    title: "Spider Man",
+    poster: "https://img1.hotstarext.com/image/upload/f_auto,t_hcdl/sources/r1/cms/prod/2356/1282356-h-a6b1c484c811"
+  }
+
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <>
+      <IndividualMovie {...testMovie} />
+    </>
   );
 }
 
